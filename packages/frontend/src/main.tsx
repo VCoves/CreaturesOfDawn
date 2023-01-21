e5e0dac9206987cffa31df2fffe45285a5f673ab
Daniel Sintimbrean <ddsintim@inf.upv.es>
2023-01-21 14:00:10 +0100

    install wagmi

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { WagmiConfig } from "wagmi";
+import { client } from "./wagmi";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <WagmiConfig client={client}>
+      <App />
+    </WagmiConfig>
+  </React.StrictMode>
+);
